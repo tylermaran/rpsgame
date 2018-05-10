a4f700b22c0a196642da7aecdfb241720fceb1ae
Tyler Maran <tyler.maran@gmail.com>
2018-05-10 08:25:20 -0400

    oops forgot styling...

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // var firebase = require("firebase/app");
-
+var updateOnce = true;
 // Initialize Firebase
 var config = {
 	apiKey: "AIzaSyDvmP-ma3_H1RtIO-z0T5w6_Xu69bbQa1k",
@@ -10,150 +10,152 @@ var config = {
 	messagingSenderId: "166650611571"
 };
 firebase.initializeApp(config);
+
 var round = -1;
 var open = false;
 // Create a variable to reference the database
 var database = firebase.database();
 
-var gamestats = {
-	rock: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	paper: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	scissors: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	win: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-	loss: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-}
+// var gamestats = {
+// 	rock: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+// 	paper: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+// 	scissors: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+// 	win: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+// 	loss: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+// }
+
+// database.ref().set({
+// 	gamestats: gamestats
+// });
+
+
 
 var DataArray = [];
 
 $("img").on("click", function () {
-
+	updateOnce = true;
 	console.log(this.id);
 	round++;
 
-	console.log("Chances")
-	// console.log(chances)
-	readData(1, 1)
-	console.log("updated stats")
-	// console.log(updatedStats);
-	playgame(this.id, round, currentStats, chances);
+	playGame(this.id, round);
 })
 
-database.ref().on("value", function (snapshot) {
+function playGame(userPick, round) {
+	if (round === 9) {
 
-	var currentStats = snapshot.val().gamestats;
-
-	var sum = [];
-	var rockChance = [];
-	var paperChance = [];
-	var scissorChance = [];
-
-	// Set sum equal to the total for each position in Array (0,1,2,3...)
-	for (let j = 0; j < 5; j++) {
-		sum[j] = currentStats.rock[j] + currentStats.rock[j] + currentStats.rock[j]
-
-		rockChance[j] = (currentStats.rock[j] / sum[j]);
-		paperChance[j] = rockChance[j] + (currentStats.paper[j] / sum[j]);
-		scissorChance[j] = paperChance[j] + (currentStats.scissors[j] / sum[j]);
+		playAgain();
 	}
-
-	var chances = {
-		rockChance: rockChance,
-		paperChance: paperChance,
-		scissorChance: scissorChance
-	}
-
-	// If any errors are experienced, log them to console.
-}, function (errorObject) {
-	console.log("The read failed: " + errorObject.code);
-});
-
-
-function playGame(userPick, round, currentStats, chances) {
 
 	database.ref().on("value", function (snapshot) {
 
-		var currentStats = snapshot.val().gamestats;
-		var updatedStats = currentStats;
-		var sum = [];
-		var rockChance = [];
-		var paperChance = [];
-		var scissorChance = [];
+			var currentStats = snapshot.val().gamestats;
+			var updatedStats = currentStats;
+			var sum = [];
+			var rockChance = [];
+			var paperChance = [];
+			var scissorChance = [];
 
-		// Set sum equal to the total for each position in Array (0,1,2,3...)
-		for (let j = 0; j < 5; j++) {
-			sum[j] = currentStats.rock[j] + currentStats.rock[j] + currentStats.rock[j]
+			// Set sum equal to the total for each position in Array (0,1,2,3...)
+			for (let j = 0; j < 5; j++) {
+				sum[j] = currentStats.rock[j] + currentStats.rock[j] + currentStats.rock[j]
 
-			rockChance[j] = (currentStats.rock[j] / sum[j]);
-			paperChance[j] = rockChance[j] + (currentStats.paper[j] / sum[j]);
-			scissorChance[j] = paperChance[j] + (currentStats.scissors[j] / sum[j]);
-		}
+				rockChance[j] = (currentStats.rock[j] / sum[j]);
+				paperChance[j] = rockChance[j] + (currentStats.paper[j] / sum[j]);
+				scissorChance[j] = paperChance[j] + (currentStats.scissors[j] / sum[j]);
+			}
 
-		var chances = {
-			rockChance: rockChance,
-			paperChance: paperChance,
-			scissorChance: scissorChance
-		}
-
-		switch (userPick) {
-			case "rock":
-				// User Selected Rock, winning move would be paper
-				var temp = (updatedStats.paper[round]);
-				temp++;
-				console.log(temp);
-				updatedStats.paper[round] = temp;
-				// return database.ref().set({gamestats: currentStats});
-				break;
-			case "scissors":
-				// User Selected scissors, winning move would be rock
-				var temp = (updatedStats.rock[round]);
-				temp++;
-				console.log(temp);
-				updatedStats.rock[round] = temp;
-				// return database.ref().set({gamestats: currentStats});
-
-				break;
-			case "paper":
-				// User Selected Paper, winning move would be scissors
-				var temp = (updatedStats.paper[round]);
-				temp++;
-				console.log(temp);
-				updatedStats.rock[round] = temp;
-				// return database.ref().set({gamestats: currentStats});
-				break;
-			default:
-				break;
-		}
-
-		// If any errors are experienced, log them to console.
-	}, function (errorObject) {
-		console.log("The read failed: " + errorObject.code);
-	});
+			var chances = {
+				rockChance: rockChance,
+				paperChance: paperChance,
+				scissorChance: scissorChance
+			}
 
 
-	if (round > 9) {
-		alert("game over!");
-	}
+			var computerChoice;
+			var random = Math.random();
+			if (updateOnce) {
+				if (random <= 1 && random > chances.scissorChance[round]) {
+					console.log("Scissors");
+					computerChoice = "scissors";
 
-	var computerChoice;
-	var random = Math.random();
-	if (random <= 1 && random > chances.scissorChance[round]) {
-		console.log("Scissors");
-		computerChoice = "scissors";
-	} else if (random <= chances.scissorChance[round] && random > chances.paperChance[round]) {
-		console.log("Paper");
-		computerChoice = "paper";
-	} else {
-		console.log("Rock");
-		computerChoice = "rock";
-	}
+					$("#wat").attr("src", "./images/scissors.png");
+
+				}
+				if (random <= chances.scissorChance[round] && random > chances.paperChance[round]) {
+					console.log("Paper");
+					computerChoice = "paper";
+					// var image = $("<img>");
+					$("#wat").attr("src", "./images/paper.png");
+
+				}
+				if (random <= chances.paperChance[round] && random >= 0) {
+					console.log("Rock");
+					computerChoice = "rock";
+
+					$("#wat").attr("src", "./images/rock.png");
+
+				}
+			}
 
 
-	$(".round").text("Round " + (round + 1));
+			switch (userPick) {
+				case "rock":
+					// User Selected Rock, winning move would be paper
+					var temp = (updatedStats.paper[round]);
+					temp++;
+					console.log(temp);
+					updatedStats.paper[round] = temp;
+
+
+					if (updateOnce) {
+						updateOnce = false;
+						database.ref().set({
+							gamestats: updatedStats
+						});
+					}
+
+					break;
+				case "scissors":
+					// User Selected scissors, winning move would be rock
+					var temp = (updatedStats.rock[round]);
+					temp++;
+					console.log(temp);
+					updatedStats.rock[round] = temp;
+					if (updateOnce) {
+						updateOnce = false;
+						database.ref().set({
+							gamestats: updatedStats
+						});
+					}
+
+					break;
+				case "paper":
+					// User Selected Paper, winning move would be scissors
+					var temp = (updatedStats.paper[round]);
+					temp++;
+					console.log(temp);
+					updatedStats.scissors[round] = temp;
+					if (updateOnce) {
+						updateOnce = false;
+						database.ref().set({
+							gamestats: updatedStats
+						});
+					}
+					break;
+				default:
+					break;
+			}
+
+			$(".round").text("Round " + (round + 1));
+
+			// If any errors are experienced, log them to console.
+		},
+		function (errorObject) {
+			console.log("The read failed: " + errorObject.code);
+		});
+
 }
 
-function setData(updatedStats) {
-	console.log(updatedStats);
-}
 
 // Whenever a user clicks the open button
 $(".open").on("click", function () {
@@ -203,3 +205,9 @@ $(".open").on("click", function () {
 	// Prevent default behavior
 	event.preventDefault();
 });
+
+
+function playAgain() {
+	alert("game over!");
+	round = -1;
+}
